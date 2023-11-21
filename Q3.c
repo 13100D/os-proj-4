5dcb6725ccc80dcf9453438efc8ca328874d26d1
13100d <3lithium6.9@gmail.com>
2023-11-21 18:20:39 +0530

    add thread building and shi

--- Q3.c
+++ Q3.c
@@ -1,5 +1,32 @@
-void* left(void* args)                        // cars on the left
+#include <stdio.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <unistd.h>
 
-void* right(void* args)                // cars on the right
+void* left(void* args){}                  // cars on the left
 
-void passing(int dir) 
+void* right(void* args){}             // cars on the right
+
+void passing(int dir){}
+
+
+int main() {
+    int left_cars, right_cars;
+    printf("cars on left: ");
+    scanf("%d", &left_cars);
+    printf("cars on right: ");
+    scanf("%d", &right_cars);
+    pthread_t right_car_thread[right_cars], left_car_threads[left_cars];
+    for (int i = 0; i < left_cars; ++i) {
+        pthread_create(&left_car_threads[i], NULL, left, NULL);
+    }
+    for (int i = 0; i < right_cars; ++i) {
+        pthread_create(&right_car_thread[i], NULL, right, NULL);
+    }
+    for (int i = 0; i < left_cars; ++i) {
+        pthread_join(left_car_threads[i], NULL);
+    }
+    for (int i = 0; i < right_cars; ++i) {
+        pthread_join(right_car_thread[i], NULL);
+    }
+}
