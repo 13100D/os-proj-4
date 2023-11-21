056472b6a4b23f8f9ed082a0b92cdbf8e8f786d9
13100d <3lithium6.9@gmail.com>
2023-11-21 17:24:48 +0530

    remove comments

--- q2.c
+++ q2.c
@@ -4,7 +4,7 @@
 #include <unistd.h>
 
 
-sem_t boardable_mutex, full_mutex, end_journey_mutex,boardedcountmutex;
+sem_t boardable_mutex, full_mutex, end_journey_mutex, boardedcountmutex, unboarded;
 int passengers_boarded = 0;
 int car_capacity;
 int total_passengers;
@@ -32,28 +32,18 @@ void offboard(int passenger_id) {
 }
 void* car(void* args) {
     while (1) {
-        // Load passengers
         load();
-
-        // Signal passengers that the car is boardable
         for (int i = 0; i < car_capacity; ++i) {
             sem_post(&boardable_mutex);
         }
-
-        // Wait until the car is full or there are no more passengers
-        sem_wait(&full_mutex);
-
-        // Run the duration of the ride
+	sem_wait(&full_mutex);
         printf("Car is on the ride.\n");
-        sleep(2);  // Simulating the duration of the ride
-
-        // Unload passengers
-        unload();
-
-        // Signal passengers that the journey has ended
+        sleep(2);
+	unload();
         for (int i = 0; i < car_capacity; ++i) {
             sem_post(&end_journey_mutex);
         }
+	sem_wait(&unboarded);
     }
 }
 
@@ -61,26 +51,16 @@ void* passenger(void* args) {
     int passenger_id = *((int*)args);
 
     while (1) {
-        // Wait until the car is boardable
-        sem_wait(&boardable_mutex);
-
-        // Board the car
-        board(passenger_id);
+	sem_wait(&boardable_mutex);
+	board(passenger_id);
         sem_wait(&boardedcountmutex);
         passengers_boarded++;
         sem_post(&boardedcountmutex);
-        // If the car is full or no more passengers are waiting, signal the car
-        if (passengers_boarded == car_capacity || passengers_waiting == 0) {
+	if (passengers_boarded == car_capacity || passengers_waiting == 0) {
             sem_post(&full_mutex);
         }
-
-        // Wait for the end of the journey
         sem_wait(&end_journey_mutex);
-
-        // Get off the car
         offboard(passenger_id);
-
-        // Reset passengers_boarded count
         if (passengers_boarded == car_capacity) {
             passengers_boarded = 0;
         }
@@ -89,14 +69,10 @@ void* passenger(void* args) {
 
 
 int main() {
-
-
-    // Initialize semaphores
     sem_init(&boardable_mutex, 0, 0);
     sem_init(&full_mutex, 0, 0);
     sem_init(&end_journey_mutex, 0, 0);
     sem_init(&boardedcountmutex,0,1);
-    // Take car capacity and total passengers as input
     printf("Enter car capacity: ");
     scanf("%d", &car_capacity);
 
@@ -110,25 +86,15 @@ int main() {
     int passenger_ids[total_passengers];
     passengers_waiting = total_passengers;
     passengers_remaining = total_passengers;
-
-    // Create car thread
     pthread_create(&car_thread, NULL, car, NULL);
-
-    // Create passenger threads
     for (int i = 0; i < total_passengers; ++i) {
         passenger_ids[i] = i + 1;
         pthread_create(&passenger_threads[i], NULL, passenger, &passenger_ids[i]);
     }
-
-    // Join passenger threads
     for (int i = 0; i < total_passengers; ++i) {
         pthread_join(passenger_threads[i], NULL);
     }
-
-    // Join the car thread
     pthread_join(car_thread, NULL);
-
-    // Destroy semaphores
     sem_destroy(&boardable_mutex);
     sem_destroy(&full_mutex);
     sem_destroy(&end_journey_mutex);
