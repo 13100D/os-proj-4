06d55ac83e3c8ddf4fdb50fad17d246b0bd3997e
13100d <3lithium6.9@gmail.com>
2023-11-21 23:08:49 +0530

    dac lagni aaj to

--- Q3.c
+++ Q3.c
@@ -10,13 +10,13 @@ sem_t left_go, right_go, bridge_avail, bridgecountvaravail;
 
 
 
-void passing(int dir){
-    char arr[2][50] = {"car go nyoom LEFT<---RIGHT\n", "car go nyoom RIGHT--->LEFT\n"};
+void passing(int dir, int id){
+    char arr[2][50] = {" go nyoom LEFT<---RIGHT\n", " go nyoom RIGHT--->LEFT\n"};
     sem_wait(&bridgecountvaravail);
     cars_on_bridge++;
     sem_post(&bridgecountvaravail);
     sleep(3);
-    printf("%s", arr[dir]);
+    printf("car %d%s",id, arr[dir]);
     sem_wait(&bridgecountvaravail);
     cars_on_bridge--;
     if(cars_on_bridge == 0){
@@ -28,14 +28,16 @@ void passing(int dir){
 
 void* left(void* args){
     //wait for some signal to go
+    int car_id = *((int*)args);
     sem_wait(&left_go);
-    passing(1);
+    passing(1, car_id);
 }                  // cars on the left
 
 void* right(void* args){
     //wait for some signal to go
+    int car_id = *((int*)args);
     sem_wait(&right_go);
-    passing(0);
+    passing(0, car_id);
 }             // cars on the right
 
 int main() {
@@ -43,7 +45,7 @@ int main() {
     scanf("%d", &left_cars);
     printf("cars on right: ");
     scanf("%d", &right_cars);
-
+    int leftcarids[left_cars], rightcarids[right_cars];
 
     pthread_t right_car_thread[right_cars], left_car_threads[left_cars];
     sem_init(&left_go, 0, 0);
@@ -51,10 +53,12 @@ int main() {
     sem_init(&bridge_avail, 0, 1);
     sem_init(&bridgecountvaravail, 0, 1);
     for (int i = 0; i < left_cars; ++i) {
-        pthread_create(&left_car_threads[i], NULL, left, NULL);
+        leftcarids[i] = i+1;
+        pthread_create(&left_car_threads[i], NULL, left, &leftcarids[i]);
     }
     for (int i = 0; i < right_cars; ++i) {
-        pthread_create(&right_car_thread[i], NULL, right, NULL);
+        rightcarids[i] = i+1;
+        pthread_create(&right_car_thread[i], NULL, right, &rightcarids[i]);
     }
 
 
@@ -66,7 +70,7 @@ int main() {
         }
 
     }
-    if (left_cars % max_cars != 0) {
+    if (left_cars % max_cars > 0) {
         sem_wait(&bridge_avail);
         printf("--------------------\n");
         for (int i = 0; i < left_cars % max_cars; i++) {
@@ -81,10 +85,12 @@ int main() {
         }
 
     }
-    sem_wait(&bridge_avail);
-    printf("--------------------\n");
-    for(int i=0; i<right_cars%max_cars; i++){
-        sem_post(&right_go);
+    if(right_cars%max_cars > 0){
+        sem_wait(&bridge_avail);
+        printf("--------------------\n");
+        for(int i=0; i<right_cars%max_cars; i++){
+            sem_post(&right_go);
+        }
     }
 
     for (int i = 0; i < left_cars; ++i) {
@@ -92,5 +98,8 @@ int main() {
     }
     for (int i = 0; i < right_cars; ++i) {
         pthread_join(right_car_thread[i], NULL);
+    }
+    if(left_cars <= 0 && right_cars <= 0){
+        printf("mai mu nahi lagta aisi value dene valo ke\n");
     }
 }
